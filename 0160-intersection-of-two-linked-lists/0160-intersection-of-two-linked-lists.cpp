83432a0bcc5e9c73fc2bced141d26ee4f1566df5
Ahmed Hany Anwar <134737874+ahmedhanyanwar@users.noreply.github.com>
2025-07-10 16:34:30 +0300

    Time: 38 ms (49.48%), Space: 18.6 MB (52.88%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -6,18 +6,6 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-#define LC_HACK
-#ifdef LC_HACK
-const auto __ = []()
-{
-    struct ___
-    {
-        static void _() { std::ofstream("display_runtime.txt") << 0 << '\n'; }
-    };
-    std::atexit(&___::_);
-    return 0;
-}();
-#endif
 
 class Solution {
 public:
